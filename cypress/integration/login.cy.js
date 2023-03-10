2b2c3745c0e8f860dfaba388281b0d74817016d3
Olawale <olapetanwale@outlook.com>
2023-03-10 10:49:26 +0100

    Add log in test script

--- cypress/integration/login.cy.js
+++ cypress/integration/login.cy.js
@@ -13,6 +13,6 @@ describe('Login oage', () => {
   })
   it('user is unable to log with wrong credentials', () => {
     cy.get('input[name=username]').type('Admin')
-    cy.get('input[name=password]').type('admin124{enter}')
+    cy.get('input[name=password]').type('admin123{enter}')
   })
 })
